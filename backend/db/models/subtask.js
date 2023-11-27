f39a17a6d5f77577e573cac47274dfd724a60d8a
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2023-11-27 15:58:08 +0000

    added: SubTask model and migration

---
+++ backend/db/models/subtask.js
@@ -0,0 +1,41 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class SubTask extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  SubTask.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      isCompleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      taskId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Task",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "SubTask",
+    }
+  );
+  return SubTask;
+};
