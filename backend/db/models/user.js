603dad1a067e211f14710f7624e0963184406593
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2023-11-27 19:38:13 +0000

    added: methods for auth in User model

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,5 +1,6 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, Op } = require("sequelize");
+const bcrypt = require("bcrypt");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -7,6 +8,44 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+
+    static async createUser({ username, email, password }) {
+      // Create a new user if it does not exist
+      const user = await User.findOne({
+        where: {
+          [Op.or]: [{ username }, { email }],
+        },
+      });
+
+      if (user) {
+        return false;
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      console.log("hashed Password: ", hashedPassword);
+      const newUser = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+      });
+
+      return newUser.scope("currentUser").findByPk(newUser.id);
+    }
+
+    static async login({ credential, password }) {
+      const user = await User.scope("loginUser").findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential,
+          },
+        },
+      });
+
+      if (user && (await bcrypt.compare(password, user.password))) {
+        return await user.scope("currentUser").findByPk(user.id);
+      }
+    }
     static associate(models) {
       // define association here
       User.hasMany(models.Board, {
@@ -50,6 +89,21 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "User",
+      defaultScope: {
+        attributes: {
+          exclude: ["password", "createdAt", "updatedAt"],
+        },
+      },
+      scopes: {
+        loginUser: {
+          attributes: {},
+        },
+        currentUser: {
+          attributes: {
+            exclude: ["password"],
+          },
+        },
+      },
     }
   );
   return User;
