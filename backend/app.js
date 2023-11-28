a5c473a521e231b35719738e12adb625409c1519
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2023-11-28 14:03:50 +0000

    added: error handling and jwk package

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,60 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const { ValidationError } = require("sequelize");
 
 const app = express();
+const routes = require("./routes/index");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const logger = (req, _res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next();
+};
+
+app.use(logger);
+
 app.get("/live", (_req, res) => {
-  res.send("live");
+  res.status(200).json({
+    status: "OK",
+    message: "Server is live",
+  });
+});
+
+app.use(routes);
+
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, _req, _res, next) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = "Validation error";
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
 });
 
 module.exports = app;
