696d7a368e90070e43c72afb5009cf3425c69e1c
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2023-12-05 09:48:50 +0000

    fixed: task height when overflow

--- frontend/src/components/content/column/task/Task.jsx
+++ frontend/src/components/content/column/task/Task.jsx
@@ -3,7 +3,9 @@ import "./task.css";
 const Task = () => {
   return (
     <div className="task">
-      <div className="task__title">Task Title</div>
+      <div className="task__title">
+        Lorem ipsum dolor sit amet
+      </div>
       <p className="subTasks">0 of 3 subtasks</p>
     </div>
   );
