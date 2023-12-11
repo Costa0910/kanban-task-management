29cc9dd1d46f511a85db24b270cf5db234a3fa15
Armando Costa <83301015+Costa0910@users.noreply.github.com>
2023-12-11 08:36:07 +0000

    removed: packege to make api calls

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import AppProvider from "../src/context/AppContext.jsx";
 import reducer from "../src/context/reducer.js";
 import initialState from "../src/context/initialState.js";
 import "./index.css";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+
+// import Root from "./routes/Root.jsx";
+// import ErrorPage from "./routes/ErrorPage.jsx";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     errorElement: <ErrorPage />,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
